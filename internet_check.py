0c8b631ba73f6c74e6189181d524eb1aaee53449
Leonaaaaaaa <93478884+Leonaaaaaaa@users.noreply.github.com>
2023-06-11 21:51:57 +0200

    Update internet_check.py

--- internet_check.py
+++ internet_check.py
@@ -1,12 +1,25 @@
 import socket, time, datetime
 
+
 def check_connectivity():
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     online = True
     try:
-        socket.gethostbyname("www.google.com")
-    except:
-        online = False
+        PORT = 443
+        HOST = socket.gethostbyname("www.google.com")
 
+        connect = s.connect_ex((HOST, PORT))
+        
+        if connect != 0:
+            raise Exception
+        
+    except Exception as error:
+        online = False
+        print(error)
+
+    s.close()
     return online
 
 while True:
@@ -17,4 +30,4 @@ while True:
 
     print(text)
     log.write(text)
-    time.sleep(60)
+    time.sleep(2)
